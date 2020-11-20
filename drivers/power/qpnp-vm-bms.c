38d2696b46ff7bd789fd91f1e057e9a768468897
rakeshb5615 <rakesh.5615rbrb@gmail.com>
2020-11-20 14:29:03 +0000

    DRIVERS:POWER: Oppo A51f(15009) changes

--- drivers/power/qpnp-vm-bms.c
+++ drivers/power/qpnp-vm-bms.c
@@ -3577,6 +3577,16 @@ static int set_battery_data(struct qpnp_bms_chip *chip)
 			{
 				pr_err("available high atl batterydata\n");
 			}
+	} else if (is_project(OPPO_15009)) {
+		node = of_find_node_by_name(chip->spmi->dev.of_node,
+					"qcom,battery-data-sony");
+		if (!node) {
+			pr_err("No available batterydata\n");
+			return -EINVAL;
+		} else
+			{
+				pr_err("available high sony batterydata\n");
+			}
     } else {
 		node = of_find_node_by_name(chip->spmi->dev.of_node,
 					"qcom,battery-data");
